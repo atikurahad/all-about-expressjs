21fffbc1bb69c6c26991ab0e09f41d38cf56518b
unknown <atikurahad@gmail.com>
2024-08-03 07:02:06 +0600

    use router

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
- const express = require('express');
- const app = express();
- const PORT = 3001;
+const express = require("express");
+const app = express();
+const PORT = 3001;
 
+const router = express.Router();
+app.use(router);
+router.get("/", (req, res) => {
+  res.end("Hello EXPRESS");
+});
+router.post("/", (req, res) => {
+  res.end("Hello EXPRESS, I am from Post");
+});
 
- app.get('/',(req,res)=>{
-    
-    res.end("Hello EXPRESS")
-
- })
- app.post('/',(req,res)=>{
-    
-    res.end("Hello EXPRESS, I am from Post")
-
- })
-
- app.listen(PORT,()=>{
-    console.log(`App is listening on port ${PORT}`);
- })
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
