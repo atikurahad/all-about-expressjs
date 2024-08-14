d0a8f6cb98bbd0abd4dc07aab2f67499360baee0
unknown <atikurahad@gmail.com>
2024-08-14 23:11:45 +0600

    json data

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -11,7 +11,9 @@ const todoSchema = require("../schemas/todoSchema");
 const Todo = new mongoose.model("Todo", todoSchema);
 //get All the todos
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+   res.send("i am from get todo")
+});
 
 //get a todo by id
 
@@ -19,14 +21,26 @@ router.get("/id:", async (req, res) => {});
 
 //POST A todo
 
-router.post('/',async(req,res)=>{
-   let newTodo = req.body()
-   await newTodo.save()
-})
+// router.post('/',async(req,res)=>{
+//    await Todo.insertOne(req.body,()=>{
+//       if(err){
+//          res.send(500).json({
+//             err:"there was an error"
+//          })
+//       }else{
+//          res.send(200).json({
+//             message:"there was no  error"
+//          })
+//       }
+//    })
+// })
 
 //POST multiple todo
 
-router.post("/all", async (req, res) => {});
+router.post("/all", async (req, res) => {
+
+   
+});
 //PUT todo
 
 router.put("/id:", async (req, res) => {});
